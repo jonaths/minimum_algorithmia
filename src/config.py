7d60391c2cf2642f856980d6f4a652898a411c1d
jonathan <jonathan.serrano@ext.cemex.com>
2022-09-22 12:56:56 -0500

    APP NAME en config de log.

--- src/config.py
+++ src/config.py
@@ -7,10 +7,12 @@ load_dotenv(verbose=True)
 # Poner aquí cualquier otra variable que haya que cargar del archivo .env
 # En el caso de Algorithmia en lugar de un .env se setea un Secret desde la aplicación web
 
+APP_NAME = os.getenv('APP_NAME', 'MY_APP')
+
 ENV_VAR = os.getenv('ENV_VAR')
 STORAGE_LOGS = os.getenv('STORAGE_LOGS')
 
 PAPERTRAIL_ADDRESS = os.getenv('PAPERTRAIL_ADDRESS')
 PAPERTRAIL_UDP_PORT = os.getenv('PAPERTRAIL_UDP_PORT')
 
-logger = papertrail_logging(address=(PAPERTRAIL_ADDRESS, int(PAPERTRAIL_UDP_PORT)))
+logger = papertrail_logging(address=(PAPERTRAIL_ADDRESS, int(PAPERTRAIL_UDP_PORT)), app_name=APP_NAME)
